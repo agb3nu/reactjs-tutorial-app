94daa3e1212529c8c37ff6637fa6162a6f3558ef
agb3nu <agb3nu@gmail.com>
2021-04-05 12:52:26 +0000

    finished another lessons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
+import  Navbar from './Navbar';
+import  Home from './Home';
 
 function App() {
 
   const title = "Agbenu demo";
   const num = 60;
-  const pple = {name: 'Eli' , age: 8 }
+  const pple = {name: 'Eli' , age: 8 };
+  const link = "http://www.agbenu.com";
+
 
   return (
     <div className="App">
-      <h3>{ title }</h3>
-      <p>Liked { num } times </p>
-      <hr/>
-      <h3>{ "Static string" }</h3>
-      <p> { Math.random() * 4 } </p>
-      <p> { [ 4, 8, 12, 16 , 20 ] } </p>
+      <Navbar />
+      <div className="content">
+
+        <Home />
+        <h3>{ title }</h3>
+        <p>Liked { num } times </p>
+        <hr/>
+        <h3>{ "Static string" }</h3>
+        <p> { Math.random() * 4 } </p>
+        <p> { [ 4, 8, 12, 16 , 20 ] } </p>
+
+        <a href={link}>Agbenu page</a>
+
+        <hr/>
+        
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<h2>agbenu navbar demo</h2>
+				<div className="links">
+					<a href="/">Home</a>
+					<a href="/create">New post</a>	
+				</div>
+		</nav>
+	 );
+}
+
+export default Navbar;
